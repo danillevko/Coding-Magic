38401fb5400523eed46ce6e4bd16f33b0e66b28c
danillevko <danillevko1212@gmail.com>
2025-12-25 13:36:13 -0700

    cards

--- magic.js
+++ magic.js
@@ -171,9 +171,15 @@ timeBtn.addEventListener('click', ()=>{
 
 const socerBall = document.querySelector('#socer-ball');
 const socerField = document.querySelector('#socer-field');
-socerBall.addEventListener('mouseover', ()=>{
+let mouseX = 0;
+let mouseY = 0;
+socerBall.addEventListener('mouseover', (event)=>{
     let ballPos = socerBall.getBoundingClientRect();
     let fieldPos = socerField.getBoundingClientRect();
+    mouseX = event.clientX - fieldPos.left;
+    mouseY = event.clientY - fieldPos.top;
+    socerBall.style.left = (mouseX - 25) + 'px';
+    socerBall.style.top = (mouseY - 25) + 'px';
 })
 
 
@@ -213,13 +219,110 @@ prevBtn.addEventListener('click', ()=>{
 });
 
 
+// SCIENTISTS
+
+const scientists = [ 
+    { 
+        name: "Albert", 
+        surname: "Einstein", 
+        born: 1879, 
+        dead: 1955, 
+        id: 1 
+    }, 
+    { 
+        name: "Isaac", 
+        surname: "Newton", 
+        born: 1643, 
+        dead: 1727, 
+        id: 2 
+    }, 
+    { 
+        name: "Galileo", 
+        surname: "Galilei", 
+        born: 1564, 
+        dead: 1642, 
+        id: 3 
+    }, 
+    { 
+        name: "Marie", 
+        surname: "Curie", 
+        born: 1867, 
+        dead: 1934, 
+        id: 4 
+    }, 
+    { 
+        name: "Johannes", 
+        surname: "Kepler", 
+        born: 1571, 
+        dead: 1630, 
+        id: 5 
+    }, 
+    { 
+        name: "Nicolaus", 
+        surname: "Copernicus", 
+        born: 1473, 
+        dead: 1543, 
+        id: 6 
+    }, 
+    { 
+        name: "Max", 
+        surname: "Planck", 
+        born: 1858, 
+        dead: 1947, 
+        id: 7 
+    }, 
+    { 
+        name: "Katherine", 
+        surname: "Blodgett", 
+        born: 1898, 
+        dead: 1979, 
+        id: 8 
+    }, 
+    { 
+        name: "Ada", 
+        surname: "Lovelace", 
+        born: 1815, 
+        dead: 1852, 
+        id: 9 
+    }, 
+    { 
+        name: "Sarah E.", 
+        surname: "Goode", 
+        born: 1855, 
+        dead: 1905, 
+        id: 10 
+    }, 
+    { 
+        name: "Lise", 
+        surname: "Meitner", 
+        born: 1878, 
+        dead: 1968, 
+        id: 11 
+    }, 
+    { 
+        name: "Hanna", 
+        surname: "Hammarström", 
+        born: 1829, 
+        dead: 1909, 
+        id: 12 
+    } 
+];
+
+const cards = document.querySelector('.cards');
+scientists.forEach(scientists => {
+    const li = document.createElement('li');
+    li.textContent = `${scientists.name} ${scientists.surname} ${scientists.born}-${scientists.dead}`;
+    cards.append(li);
+});
+
+
 // FOOTER
 
 const footerInput = document.querySelector('.footer-email');
 const subBtn = document.querySelector('.footer-sub-btn');
 subBtn.addEventListener('click', ()=>{
     footerInput.value = null;
-})
+});
 
 
 
